e7063accfbabf3504455aabfa71f153280992427
samrat1223 <samratganguly37@gmail.com>
2021-10-14 15:04:18 +0530

    update feature added

--- src/main/java/com/samrat/SpringBootThymeleaf_crud/controller/EmployeeController.java
+++ src/main/java/com/samrat/SpringBootThymeleaf_crud/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -36,5 +37,16 @@ public class EmployeeController {
         //save employee to database
         employeeService.saveEmployee(employee);
         return "redirect:/";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable (value = "id") long id, Model model)
+    {
+        //Get employee from service
+        Employee employee = employeeService.getEmployeeById(id);
+
+        //set employee as a model attribute to pre-populate the form
+        model.addAttribute("employee",employee);
+        return  "update_employee";
     }
 }
